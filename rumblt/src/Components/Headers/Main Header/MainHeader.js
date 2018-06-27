392ef7aade0f667213da8004ff51bdd1146f4a2e
Benjamin Alldredge <benjaminalldredge@Benjamins-MacBook-Pro.local>
2018-06-27 17:01:09 -0600

    benjamin's 2nd commit Wednesday

--- rumblt/src/Components/Headers/Main Header/MainHeader.js
+++ rumblt/src/Components/Headers/Main Header/MainHeader.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './MainHeader.css'
 import compass from './icons/compass.svg'
 import home from './icons/home.svg';
@@ -20,20 +21,20 @@ export default class MainHeader extends Component{
         return(
             <div id='navBar'>
                 <div id='logoContainer'>
-                    <img id='logo' src={logo} alt=""/>
+                    <Link><img id='logo' src={logo} alt="Link to dashboard"/></Link>
                     <div id="search">
                     <div id="searchbar">
-                    <img className='glass' src={search} alt=""/>
+                    <img className='glass' src={search} alt="Search icon"/>
                     Search rumblt
                     </div>
                     </div>
                 </div>
                 <div id='iconContainer'>
-               <img id='icon' src={home} alt=""/>
-               <img id='icon' src={compass} alt=""/>
-               <img id='icon' src={message} alt=""/>
-               <img id='icon' src={profile} alt=""/>
-               <img id='edit' src={edit} alt=""/>
+                <Link to='/dashboard'><img id='icon' src={home} alt=""/></Link>
+                <Link to='/explore'><img id='icon' src={compass} alt=""/></Link>
+                <img id='icon' src={message} alt=""/>
+                <Link to='/profile/:username'><img id='icon' src={profile} alt=""/></Link>
+                <img id='edit' src={edit} alt=""/>
                 </div>
             </div>
         )
