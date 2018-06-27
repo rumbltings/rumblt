17af3613d2d1185c391f3e76a306751109dd1c35
Tarin Strosser <strawsir@gmail.com>
2018-06-27 15:38:17 -0600

    Begun styling login page

--- rumblt/src/Components/Headers/Login Header/LoginHeader.js
+++ rumblt/src/Components/Headers/Login Header/LoginHeader.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
-import './LoginHeader.css'
-import compass from './icons/compass.svg'
-import home from './icons/home.svg';
-import message from './icons/message.svg';
-import profile from './icons/profile.svg';
-import edit from './icons/edit.svg';
+import './LoginHeader.css';
 import logo from './icons/logo.svg';
+import search from './icons/search.svg';
 
 
 
@@ -17,17 +13,25 @@ export default class LoginHeader extends Component{
 
     render(){
         return(
-            <div id='navBar'>
-                <div id='logoContainer'>
+            <div id='loginnavBar'>
+                <div id='loginlogoContainer'>
                     <img id='logo' src={logo} alt=""/>
+                    <div id="search">
+                    <div id="searchbar">
+                    <img className='glass' src={search} alt=""/>
+                    Search rumblt
+                    </div>
+                    </div>
+                    <div className="buttons">
+                <div className="signup">
+                Sign Up
                 </div>
-                <div id='iconContainer'>
-               <img id='icon' src={home} alt=""/>
-               <img id='icon' src={compass} alt=""/>
-               <img id='icon' src={message} alt=""/>
-               <img id='icon' src={profile} alt=""/>
-               <img id='edit' src={edit} alt=""/>
+                <div className="login">
+                Log in
                 </div>
+                </div>
+                </div>
+               
             </div>
         )
     }
--- rumblt/src/Components/Login/Login.js
+++ rumblt/src/Components/Login/Login.js
@@ -1,23 +1,66 @@
 import React, {Component} from 'react';
-import LoginHeader from '../Headers/Login Header/LoginHeader'
-
+import axios from 'axios'
+import LoginHeader from '../Headers/Login Header/LoginHeader';
+import rumblt from '../Headers/Login Header/icons/rumblt.svg';
 import './Login.css'
 
 export default class Login extends Component{
     constructor(){
         super()
+        this.state={
+            image: ''
+        }
 
+    }
+
+    componentDidMount(){
+        this.getRandomImage();
+    }
+
+    getRandomImage(){
+        var queries = ['shiba', 'doge', 'meme', 'art', 'goals', 'anime', 'cats', 'funny', 'adventuretime', 'tumblr', 'disney', 'food'];
+
+        var query= queries[Math.floor(Math.random()*queries.length)]
+
+        axios.get(`https://api.giphy.com/v1/gifs/random?api_key=lQiHuWLfjlMKb4krrEQar6RKMizcigD3&tag=${query}&rating=PG`).then(res=>{
+            console.log(res.data.data.image_url)
+            this.setState({image:res.data.data.image_url})
+            document.body.background = this.state.image;
+            document.body.style.backgroundSize = "cover";
+        })
     }
 
     render(){
         return(
             <div id="loginMain">
 
+
                 <header>
                 <LoginHeader/>
+                
                 </header>
-
-                LOGIN
+                <body>
+                    
+                <div id="mainsignin">
+                <div id="fulllogo">
+                <img src={rumblt} alt=""/>
+                </div>
+                <div id="subtitle">
+                <div className="subtop">
+                Come for what you discover.
+                </div>
+                Stay for what you love.
+                <div className="subbottom">
+                </div>
+                </div>
+                <div id="getstarted">
+                Get Started
+                </div>
+                <div id="loginbutton">
+                Login
+                </div>
+                </div>
+                </body>
             </div>
         )
     }
