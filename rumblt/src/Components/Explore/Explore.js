4fe311c297dd541325f092ca03ce5fbd6c2c493b
Tarin Strosser <strawsir@gmail.com>
2018-06-28 11:17:42 -0600

    Started Explore Page

--- rumblt/src/Components/Explore/Explore.js
+++ rumblt/src/Components/Explore/Explore.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
+import MainHeader from '../Headers/Main Header/MainHeader'
+import "../Explore/Explore.css"
 
 
 export default class Explore extends Component{
     constructor(){
         super()
+        this.state={
+            currentUser:{
+                
+            }
+        }
 
+    }
+
+    componentDidMount(){
+        document.body.background = '#36465d';
     }
 
     render(){
         return(
-            <div>
+            <div id="exploremain">
+                <header>
+                    <MainHeader/>
+                </header>
                 Explore
             </div>
         )
