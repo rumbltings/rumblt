4b1ed892ed529e156241d5f6e5e24d0df13a59ce
Tarin Strosser <strawsir@gmail.com>
2018-07-03 16:53:59 -0600

    Tested Dummy Data to ensure feed displays correctly

--- rumblt/src/Components/DashBoard/Dashboard.js
+++ rumblt/src/Components/DashBoard/Dashboard.js
@@ -29,7 +29,41 @@ export class Dashboard extends Component{
         super()
         this.state={
             posts: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-            isDashCurrent: false
+            isDashCurrent: false,
+            dummyData: [{
+                id: 1,
+                posterImage: 'https://78.media.tumblr.com/e61a924e988536de900ac6c3a9ef97fa/tumblr_os5r54nmOz1tcslufo1_400.png',
+                posterName: 'Poster 1',
+                postCaption: 'Hello! This is My Post!',
+                postContent: 'https://pics.me.me/i-wish-i-was-a-shibe-3398650.png',
+                notes: 123
+            },
+            {
+                id: 2,
+                posterImage: 'https://78.media.tumblr.com/6c039a0bb30eddc57524f8d490036da3/tumblr_ofzb7eKvce1ukzs7qo1_500.png',
+                posterName: 'Poster 2',
+                postCaption: 'Hello! This is My Post!',
+                postContent: 'http://www.mypokecard.com/my/galery/p7o782CtWOsk.jpg',
+                notes: 0
+            },
+            {
+                id: 3,
+                posterImage: 'https://78.media.tumblr.com/3b047b40120348466074a3491ce4fe6c/tumblr_oqtvlgp87H1t4rhclo3_400.png',
+                posterName: 'Poster 3',
+                postCaption: 'Hello! This is My Post!',
+                postContent: 'https://pics.me.me/i-wish-i-was-a-shibe-3398650.png',
+                notes: 3798
+            },
+            {
+                id: 4,
+                posterImage: 'https://78.media.tumblr.com/670412ed8df88f5978159e86e2333eff/tumblr_p2vwm5d72t1uskm6lo1_500.jpg',
+                posterName: 'Poster 4',
+                postCaption: 'Hello! This is My Post!',
+                postContent: 'http://31.media.tumblr.com/3bd9841af890cda9fcb248719385443e/tumblr_msy3raNtLP1shivooo1_400.gif',
+                notes: 10
+            }   
+        ]
+
         }
 
     }
@@ -118,10 +152,10 @@ export class Dashboard extends Component{
                 </div>
 
                     <div className="feed">
-                    {this.state.posts.map(post=>{
+                    {this.state.dummyData.map(post=>{
                         return (
                             <div key={post}>
-                                <DashFeed/>
+                                <DashFeed {...post}/>
                             </div>
                         )
                     })}
