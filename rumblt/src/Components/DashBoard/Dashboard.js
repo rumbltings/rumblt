e361e8d18dded6bcdd78f0d22d933ff2551ea882
robyn <robyn@s56.net>
2018-07-03 15:17:35 -0600

    g

--- rumblt/src/Components/DashBoard/Dashboard.js
+++ rumblt/src/Components/DashBoard/Dashboard.js
@@ -23,7 +23,6 @@ export class Dashboard extends Component{
         this.state={
             posts: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
             isDashCurrent: false,
-            currentUID: ''
         }
 
     }
